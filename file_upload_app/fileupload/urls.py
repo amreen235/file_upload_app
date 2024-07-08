ea52688dc35cf96bf84d98e33dfa7f8aa334851c
ajxv <56473062+ajxv@users.noreply.github.com>
2024-07-08 14:36:08 +0530

    report display fixed

--- file_upload_app/fileupload/urls.py
+++ file_upload_app/fileupload/urls.py
@@ -6,13 +6,9 @@ from . import views
 
 urlpatterns = [
     path('', views.upload_file, name='upload_file'),
-    path('upload/', views.display_excel_data, name='main'), 
-    path('file/<int:file_id>/', views.file_uploaded, name='file_uploaded'),
-    path('report_display/', views.report_display, name='report_display'),
-    path('BatchName_display/', views.BatchName_display, name='BatchName_display'),
-    path('SubjectName_display/', views.SubjectName_display, name='SubjectName_display'),
+    path('generate_report/', views.generate_report, name='generate_report'),
     path('get_batch_names/', views.get_batch_names, name='get_batch_names'),
     path('get_subject_names/', views.get_subject_names, name='get_subject_names'),
-    path('filter_data/', views.display_sorted_data, name='display_sorted_data'),
+    path('report/', views.filtered_report, name='report'),
     
 ]
--- file_upload_app/fileupload/views.py
+++ file_upload_app/fileupload/views.py
@@ -1,8 +1,6 @@
-
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, JsonResponse
-from .models import uploaded_data, uploaded_data_truncated_og, data_sorted
+from .models import uploaded_data
 from .forms import FileUploadForm
 from django.contrib import messages
 import pandas as pd
@@ -10,13 +8,18 @@ import sqlite3
 
 def get_batch_names(request):
     report_name = request.GET.get('report_name')
-    batch_names = uploaded_data_truncated_og.objects.filter(report_name=report_name).values_list('batch_name', flat=True).distinct()
+    batch_names = uploaded_data.objects.filter(
+        report_name=report_name, batch_name__isnull=False).values_list('batch_name', flat=True).distinct()
     return JsonResponse({'batch_names': list(batch_names)})
+
 
 def get_subject_names(request):
     batch_name = request.GET.get('batch_name')
-    subject_names = uploaded_data_truncated_og.objects.filter(batch_name=batch_name).values_list('subject_name', flat=True).distinct()
+    subject_names = uploaded_data.objects.filter(
+        batch_name=batch_name, subject_name__isnull=False).values_list('subject_name', 'subject_code').distinct()
+    
     return JsonResponse({'subject_names': list(subject_names)})
+
 
 def load_data(df, sqlite_file_path, table_name, is_ug, report_name):
     column_mapping = {
@@ -56,75 +59,11 @@ def load_data(df, sqlite_file_path, table_name, is_ug, report_name):
     df = df.sort_values(by='roll_no')
     df['obt_marks'] = df.apply(lambda x: '' if (x['exam_type'] in ['Aggregate', 'SA'] and x['obt_marks'] == 0) else x['obt_marks'], axis=1)
 
-    truncated_df = df[['exam_code', 'student_batch_name', 'batch_name', 'student_name', 'roll_no', 'exam_type',
-                       'subject_name', 'obt_marks', 'max_marks', 'obt_grade', 'rv_marks', 'rv_updated',
-                       'course_category', 'report_name']]
-
     conn = sqlite3.connect(sqlite_file_path)
     df.to_sql(table_name, conn, if_exists='append', index=False)
     conn.close()
 
-    load_truncated_data(truncated_df, "db.sqlite3", "fileupload_uploaded_data_truncated_og")
 
-    aggregate_df = truncated_df[truncated_df['exam_type'] == 'Aggregate']
-
-    # Create a dictionary to map (Student Name, Subject Name) to Obt Grade
-    grade_map = {}
-    for index, row in aggregate_df.iterrows():
-        key = (row['student_name'], row['subject_name'])
-        grade_map[key] = row['obt_grade']
-
-    # Function to fetch grade based on (Student Name, Subject Name)
-    def fetch_grade(student_name, subject_name):
-        key = (student_name, subject_name)
-        return grade_map.get(key, '')
-
-    sorted_df = pd.DataFrame()
-
-    sorted_df['fa_marks'] = pd.NA
-    sorted_df['sa_marks'] = pd.NA
-    sorted_df['agg_marks'] = pd.NA
-    # Populate the sorted_df with data from the original DataFrame
-    rows = []
-    for student in truncated_df['student_name'].unique():
-        student_data = truncated_df[truncated_df['student_name'] == student]
-        for subject in student_data['subject_name'].unique():
-            subject_data = student_data[student_data['subject_name'] == subject]
-            if not subject_data.empty:
-                row = {
-                    'roll_no': subject_data.iloc[0]['roll_no'],
-                    'student_name': student,
-                    'batch_name': subject_data.iloc[0]['batch_name'],
-                    'subject_name': subject,  # Add this line
-                    'fa_marks': subject_data[subject_data['exam_type'] == 'FA']['obt_marks'].values[0] if not subject_data[subject_data['exam_type'] == 'FA'].empty else '',
-                    'sa_marks': subject_data[subject_data['exam_type'] == 'SA']['obt_marks'].values[0] if not subject_data[subject_data['exam_type'] == 'SA'].empty else '',
-                    'agg_marks': subject_data[subject_data['exam_type'] == 'Aggregate']['obt_marks'].values[0] if not subject_data[subject_data['exam_type'] == 'Aggregate'].empty else '',
-                    'obt_grade': fetch_grade(student, subject)
-                }
-                rows.append(row)
-
-    sorted_df = pd.DataFrame(rows)
-
-    sorted_df['rv_marks'] = truncated_df['rv_marks']
-    sorted_df['rv_updated'] = truncated_df['rv_updated']
-    sorted_df['course_category'] = truncated_df['course_category']
-    sorted_df['report_name'] = truncated_df['report_name']
-
-    load_sorted_data(sorted_df, "db.sqlite3", "fileupload_data_sorted")
-
-
-def load_truncated_data(truncated_df, sqlite_file_path, table_name):
-    
-    conn = sqlite3.connect(sqlite_file_path)    
-    truncated_df.to_sql(table_name, conn, if_exists = 'append', index=False)
-    conn.close()
-    
-def load_sorted_data(truncated_df, sqlite_file_path, table_name):
-    
-    conn = sqlite3.connect(sqlite_file_path)    
-    truncated_df.to_sql(table_name, conn, if_exists = 'append', index=False)
-    conn.close()
-    
 def upload_file(request):
     if request.method == 'POST':
         form = FileUploadForm(request.POST, request.FILES)
@@ -147,47 +86,58 @@ def upload_file(request):
                 # Handle invalid case, default to False (PG)
                 is_ug = False
 
-            load_data(df, "db.sqlite3", "fileupload_uploaded_data", is_ug, report_name)
+            load_data(df, "db.sqlite3", "fileupload_uploaded_data",
+                      is_ug, report_name)
 
             messages.success(request, 'Successfully Uploaded')
-            return redirect('main')
+            return redirect('generate_report')
     else:
         form = FileUploadForm()
     return render(request, 'fileupload/upload.html', {'form': form})
 
 
-def display_excel_data(request):
-    excel_data = uploaded_data_truncated_og.objects.all()
+def generate_report(request):
+    excel_data = uploaded_data.objects.all()
 
-    reports = uploaded_data_truncated_og.objects.values_list('report_name', flat=True).distinct()
-    batch_name = uploaded_data_truncated_og.objects.values_list('batch_name', flat=True).distinct()
-    subject_name = uploaded_data_truncated_og.objects.filter(subject_name__isnull = False).values_list('subject_name', flat=True).distinct()
-    context = {'excel_data': excel_data, 
-               'reports': reports,
-               'batch_name' : batch_name,
-               'subject_name' : subject_name}
-     
-    return render(request, 'fileupload/main.html', context)
+    reports = uploaded_data.objects.values_list('report_name', flat=True).distinct()
+    context = {
+        'excel_data': excel_data,
+        'reports': reports
+    }
+
+    return render(request, 'fileupload/generate_report.html', context)
+
+def filtered_report(request):
+    report_name = request.POST['report_name']
+    batch_name = request.POST['batch_name']
+    subject_code = request.POST['subject']
+
+    filtered_data = uploaded_data.objects.filter(report_name = report_name, batch_name = batch_name, subject_code = subject_code).values()
+
+    fd_df = pd.DataFrame.from_records(filtered_data)
+
+    print(fd_df)
+
+    rows = []
+    if not fd_df.empty:
+        for roll_no in fd_df['roll_no'].unique():
+            student_data = fd_df[fd_df['roll_no'] == roll_no]
+            for subject in student_data['subject_code'].unique():
+                subject_data = student_data[student_data['subject_code'] == subject]
+                if not subject_data.empty:
+                    row = {
+                        'roll_no': roll_no,
+                        'student_name': subject_data.iloc[0]['student_name'],
+                        'batch_name': subject_data.iloc[0]['batch_name'],
+                        'subject_name': subject_data.iloc[0]['subject_name'],
+                        'fa': subject_data[subject_data['exam_type'] == 'FA']['obt_marks'].values[0] if not subject_data[subject_data['exam_type'] == 'FA'].empty else '',
+                        'sa': subject_data[subject_data['exam_type'] == 'SA']['obt_marks'].values[0] if not subject_data[subject_data['exam_type'] == 'SA'].empty else '',
+                        'aggregate': subject_data[subject_data['exam_type'] == 'Aggregate']['obt_marks'].values[0] if not subject_data[subject_data['exam_type'] == 'Aggregate'].empty else '',
+                        'obt_grade': subject_data[subject_data['exam_type'] == 'Aggregate']['obt_grade'].values[0]
+                    }
+                    rows.append(row)
     
+    print(rows)
 
-def report_display(request):
-    report_dropdown = uploaded_data_truncated_og.objects.values_list('report_name', flat=True).distinct()
-    return render(request,'fileupload/main.html', {"report_names_saved" : report_dropdown})
-
-def BatchName_display(request):
-    BatchName_dropdown = uploaded_data_truncated_og.objects.values_list('batch_name', flat=True).distinct()
-    return render(request,'fileupload/main.html', {"batch_names_saved" : BatchName_dropdown})
-
-def SubjectName_display(request):
-    SubjectName_dropdown = uploaded_data_truncated_og.objects.values_list('subject_name', flat=True).distinct()
-    # SubjectName_dropdown.delete('None')
-    return render(request,'fileupload/main.html', {"subject_names_saved" : SubjectName_dropdown})
-
-def file_uploaded(request, file_id):
-    return HttpResponse(f"File uploaded with ID: {file_id}")
-
-def display_sorted_data(request):
-    # Fetch only the specified columns
-    sorted_data = data_sorted.objects.values('roll_no', 'student_name', 'batch_name', 'subject_name', 'fa_marks', 'sa_marks', 'agg_marks', 'obt_grade')
-    context = {'sorted_data': sorted_data}
-    return render(request, 'fileupload/filter.html', context)
+    context = {'report_data': rows}
+    return render(request, 'fileupload/filtered_data.html', context)
